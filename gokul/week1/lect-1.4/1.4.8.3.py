fb73d7967f12670b6a453cc8ecad9681c7938e8c
DINESH <dinesh3212001@gmail.com>
2026-06-23 11:56:39 +0530

    perform type casting of the variable

--- gokul/week1/lect-1.4/1.4.8.3.py
+++ gokul/week1/lect-1.4/1.4.8.3.py
@@ -4,10 +4,13 @@ print(type(x))
 
 y="10.5"
 # convert y's datatype to  float
+y=float(y)
+print(type(y))
 
 
 z=10.5
 # what is the datatype of z?
+print(type(z)) #float
 
 
 # convert z to an integer
