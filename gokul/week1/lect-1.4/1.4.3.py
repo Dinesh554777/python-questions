5435b8d903ea8ba157d1a925044b7601b2bf115e
DINESH <dinesh3212001@gmail.com>
2026-06-23 11:36:09 +0530

    assigning values to the variables and performs sum and product

--- gokul/week1/lect-1.4/1.4.3.py
+++ gokul/week1/lect-1.4/1.4.3.py
@@ -7,3 +7,9 @@
 # print(a+b)
 # print(a*b)
 # ```
+a=10
+print(a) # 10 will be printed
+b=20
+print(b) # 20 will be printed
+print(a+b) # sum of a and b is 30 this will be printed
+print(a*b) # product of and b is 200 this will be printed
