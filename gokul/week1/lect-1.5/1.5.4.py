f7d84e2a07755b445b1b557fcf158fb97c662350
DINESH <dinesh3212001@gmail.com>
2026-06-23 12:41:59 +0530

    diffrence between the input() and int(input())

--- gokul/week1/lect-1.5/1.5.4.py
+++ gokul/week1/lect-1.5/1.5.4.py
@@ -1,5 +1,4 @@
 # 1. what is the difference between the below two line?
 #     ```
-#     n=input()
-#     n=int(input())
-#     ```
+n=input() #having input with string type
+n=int(input()) #having input as string and then converted into int type
