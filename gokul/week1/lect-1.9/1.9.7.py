ed642fd0e654f0c788b34b49ea12f0dd67ed01c9
DINESH <dinesh3212001@gmail.com>
2026-06-24 11:48:21 +0530

    docs: uncomment code examples for clarity on output results

--- gokul/week1/lect-1.9/1.9.7.py
+++ gokul/week1/lect-1.9/1.9.7.py
@@ -1,8 +1,6 @@
 # 1. what will be the output of  (remember B O D M A S)
-#     ```
-#     n=10+13*2
-#     print(n)
-#     m=10+13*4/2
-#     print(m)
-#     
-#     ```
+n=10+13*2
+print(n) #36
+m=10+13*4/2
+print(m) #36.0
+
