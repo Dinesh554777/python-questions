dbc86855e872ff3abb4d764b184875c31b9ea6c3
DINESH <dinesh3212001@gmail.com>
2026-06-24 11:14:47 +0530

    feat: implement area calculation for a circle based on user input

--- gokul/week1/lect-1.6/1.6.4.py
+++ gokul/week1/lect-1.6/1.6.4.py
@@ -1,1 +1,10 @@
 # 1. write a python code that takes radius of circle as input and prints the area of the circle
+
+
+import math #library for mathematical caculation
+
+radius = float(input("Enter the radius of a circle:"))
+
+area=math.pi*radius**2
+
+print(f"Area of the circle is {area}")
