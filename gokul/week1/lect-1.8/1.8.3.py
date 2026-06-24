cd64afaf9f5b9390164434f92d321484aef6ca23
DINESH <dinesh3212001@gmail.com>
2026-06-24 11:34:12 +0530

    refactor: uncomment variable assignments and print statements for clarity

--- gokul/week1/lect-1.8/1.8.3.py
+++ gokul/week1/lect-1.8/1.8.3.py
@@ -1,8 +1,12 @@
 # 1. what will be the value stored in each variable?
 #     ```
-#     a=int(5.7)
-#     b=int('10')
-#     c=int("212")
-#     d=float("1.2")
-#     e=int("1.2")
-#     ```
+a=int(5.7)
+print(a)
+b=int('10')
+print(b)
+c=int("212")
+print(c)
+d=float("1.2")
+print(d)
+#e=int("1.2")
+#print(e)
