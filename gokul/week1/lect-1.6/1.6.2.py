c514c3ceb3ac9fafed1620566b25f8a8aff2ae41
DINESH <dinesh3212001@gmail.com>
2026-06-24 11:05:23 +0530

    identify the literal and variable

--- gokul/week1/lect-1.6/1.6.2.py
+++ gokul/week1/lect-1.6/1.6.2.py
@@ -1,5 +1,7 @@
 # 1. identify the literal and variable in the below line
 #     ```
 #     name="gokul"
-#     ```
+# 
+#    ```
+name="gokul"
 # name is the variable and literal is a string type of "gokul" 
