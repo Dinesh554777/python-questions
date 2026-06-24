2893666e1409d1bc0fe18b3ac2bec5b868b0220b
DINESH <dinesh3212001@gmail.com>
2026-06-24 11:10:12 +0530

    feat: add input and output for cube volume calculation

--- gokul/week1/lect-1.6/1.6.3.py
+++ gokul/week1/lect-1.6/1.6.3.py
@@ -1,1 +1,5 @@
-# 1. Write a python code that takes side of a cube as input and prints volume of the cube as output. 
+# 1. Write a python code that takes side of a cube as input and prints volume of the cube as output.
+# 
+sides_of_cude=int(input("Enter the side of a cube:"))
+volume_of_cube=sides_of_cude**3
+print(volume_of_cube) 
