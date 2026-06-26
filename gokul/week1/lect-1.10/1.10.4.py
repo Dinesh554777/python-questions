b1c9bf89ebfe979accb89e02ac9b82281f94d6d3
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:26:02 +0530

    refactor: convert commented code to active print statements for clarity

--- gokul/week1/lect-1.10/1.10.4.py
+++ gokul/week1/lect-1.10/1.10.4.py
@@ -1,13 +1,21 @@
 # 1. what will the output of the below code be?
-#     ```
-#     print(7>3)
-#     print(7<3)
-#     print(7=3)
-#     print(7==3)
-#     print(7<=7)
-#     print(7<7)
-#     print(7<=3)
-#     print(7>=3)
-#     print(7!=3)
-#     print(a=3)
-#     ```
+
+print(7>3)
+print(7<3)
+#print(7=3) #incorrect format , so error
+print(7==3)
+print(7<=7)
+print(7<7)
+print(7<=3)
+print(7>=3)
+print(7!=3)
+#print(a=3) #incorrect format , so error
+
+# output:
+# False
+# False
+# True
+# False
+# False
+# True
+# True
