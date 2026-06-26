bc94dc30171ca65c6a9a687078cb9451bf51746d
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:44:35 +0530

    feat: uncomment code to display string multiplication and character repetition outputs

--- gokul/week1/lect-1.12/1.12.1.py
+++ gokul/week1/lect-1.12/1.12.1.py
@@ -1,6 +1,6 @@
 # 1. what will be the output?
 #     ```
-#     s='good'
-#     print(s*5)
-#     print(s[0]*5)
+s='good'
+print(s*5) # 'good'*5 = goodgoodgoodgoodgood
+print(s[0]*5) #s[0]=g, 'g'*5= ggggg
 #     ```
