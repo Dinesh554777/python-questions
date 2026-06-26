d00fa065fbb8d18b22a2bdae788797b303479575
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:32:17 +0530

    refactor: activate print statements for variable outputs and clarify comments

--- gokul/week1/lect-1.11/1.11.1.py
+++ gokul/week1/lect-1.11/1.11.1.py
@@ -1,12 +1,12 @@
 # 1. what wil be the output?
 #     ```
-# s='coffee'
-# t='bread'
-# print(s)
-# print(t)
-# print(s+t)
-# u=3
-# print(u+s)
-# print(u*t)
-# print(3.2*"gokul")
+s='coffee' 
+t='bread'
+print(s)    #coffee
+print(t)    #bread
+print(s+t)  #cofeebread
+u=3 
+#print(u+s) unsupported operand type(s) for +: 'int' and 'str'
+print(u*t)  #breadbreadbread
+# print(3.2*"gokul") can't multiply sequence by non-int of type 'float'
 #     ```
