b2f0320837fdb21ec761d9dc46951d4cc0eba181
DINESH <dinesh3212001@gmail.com>
2026-06-26 12:07:12 +0530

    feat: update string length example to include correct code and output prediction

--- gokul/week1/lect-1.12/1.12.7.py
+++ gokul/week1/lect-1.12/1.12.7.py
@@ -1,6 +1,7 @@
 # 1. let x store the value of length of the string, what will be the output?, replace LINE-2 with correct code block and predict the output
 #     ```
-#     a="gokul
-#     x=LINE-2
-#     print(a[x])
+a="gokul"
+x=len(a) #5
+# print(a[x]) #a[5] a= 0 to 4 string index out of range
+print(a[x-1])
 #     ```
