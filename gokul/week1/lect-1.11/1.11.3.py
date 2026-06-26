31ec0644ae27127f69214836000c68ae0c78ab39
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:40:07 +0530

    refactor: uncomment code for clarity and improve output visibility

--- gokul/week1/lect-1.11/1.11.3.py
+++ gokul/week1/lect-1.11/1.11.3.py
@@ -1,20 +1,20 @@
 # 1. guess the output
 #     ```
-#     s='0123456789'
-#     a=s[0]
-#     b=s[7]
-#     c=s[4]
-#     d=int(s[1])
-#     e=int(s[2])
-#     f=int(s[3])
-#     print(type(a))
-#     print(a)
-#     print(b)
-#     print(c)
-#     print(a+b+c)
-#     print("1"+"3"+"6")
-#     print(d)
-#     print(e)
-#     print(f)
-#     print(d+e+f)
+s='0123456789'
+a=s[0]
+b=s[7]
+c=s[4]
+d=int(s[1])
+e=int(s[2])
+f=int(s[3])
+print(type(a))
+print(a) #0
+print(b) #7
+print(c) #4
+print(a+b+c) #concatenate the a+b+c to give 074 
+print("1"+"3"+"6") #concatenate the string to give 136
+print(d) #1
+print(e) #2
+print(f) #3
+print(d+e+f)  #add d+e+f which is int type so 1+2+3=6
 #     ```
