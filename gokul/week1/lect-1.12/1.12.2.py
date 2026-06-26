795c5d393cdb085159a9d91cebee494309b05a80
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:47:24 +0530

    feat: uncomment code to demonstrate string equality comparisons

--- gokul/week1/lect-1.12/1.12.2.py
+++ gokul/week1/lect-1.12/1.12.2.py
@@ -1,8 +1,8 @@
 # 1. what will be the output?
 #     ```
-#     s='india'
-#     print(s=='india')
-#     print(s=='India')
-#     print(s=="India")
-#     print(s=="india")
+s='india'
+print(s=='india') #india is equla to india, true
+print(s=='India') #india is not equal to India, false 
+print(s=="India") #india is not equal to "India", false
+print(s=="india") #indian is equla to "India", True
 #     ```
