0d1cebc80458922c992a611f0e009560a13cf432
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:24:57 +0530

    fix: correct order of print statements for type evaluation

--- gokul/week1/lect-1.5/1.5.4.py
+++ gokul/week1/lect-1.5/1.5.4.py
@@ -1,4 +1,6 @@
 # 1. what is the difference between the below two line?
 #     ```
 n=input() #having input with string type
+print(type(n)) #output: <class 'str'>
 n=int(input()) #having input as string and then converted into int type
+print(type(n))
