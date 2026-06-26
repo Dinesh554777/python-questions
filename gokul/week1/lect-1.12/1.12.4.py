be6e29aa892dbd279f4fb9c75ab31947081f780f
DINESH <dinesh3212001@gmail.com>
2026-06-26 11:56:28 +0530

    feat: add explanation of lexicographic order and string comparison in Python

--- gokul/week1/lect-1.12/1.12.4.py
+++ gokul/week1/lect-1.12/1.12.4.py
@@ -1,2 +1,6 @@
 # 1. what is lexicographic order?
+'''Lexicographic order means arranging or comparing strings 
+the same way words are ordered in a dictionary.
+
+Python compares strings character by character from left to right.'''
   
