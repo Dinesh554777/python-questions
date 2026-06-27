9275d53687dc8402d6bcef8a2b24b73769263c22
DINESH <dinesh3212001@gmail.com>
2026-06-27 10:57:09 +0530

    feat: comment out examples for binary concepts for clarity

--- gokul/week1/official-questions/AQ/3.py
+++ gokul/week1/official-questions/AQ/3.py
@@ -1,8 +1,8 @@
 # In computer science, a bit is the short form of a binary digit. A bit can assume one of the two
 # values: 0 and 1. What unites the following terms?
 
-boolean -->  True and False
-state of a switch -> On and off
-outcome of a coin toss -> Head and Tail
-outcome of a tennis match -> Win or Loss
-nodding your head -> Yes or No
+# boolean -->  True and False
+# state of a switch -> On and off
+# outcome of a coin toss -> Head and Tail
+# outcome of a tennis match -> Win or Loss
+# nodding your head -> Yes or No
