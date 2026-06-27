86aa1b2862b8d54747d77013a1b6c13d2f64129b
DINESH <dinesh3212001@gmail.com>
2026-06-27 11:06:07 +0530

    feat: add calculations and print statements to find the last digit of 7 power 35

--- gokul/week1/official-questions/AQ/4.py
+++ gokul/week1/official-questions/AQ/4.py
@@ -1,2 +1,8 @@
 # Find the last digit of 7 power 35
-# 3 is last digit
+pow=7**35
+print(pow)
+pow=str(pow)
+print(type(pow))
+print(len(pow))
+last_digit=pow[29]
+print("the last digit of 7 power 35 is",last_digit)
