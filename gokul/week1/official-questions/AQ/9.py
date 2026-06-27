8b72a109ae393f1daff4a70d1c1ca10058b37aea
DINESH <dinesh3212001@gmail.com>
2026-06-27 11:17:18 +0530

    feat: add calculation and print statements for string length and repeated characters

--- gokul/week1/official-questions/AQ/9.py
+++ gokul/week1/official-questions/AQ/9.py
@@ -1,2 +1,8 @@
 # 
 # If some_string is a string of length n , how many characters does n * some_string have, where n is some positive integer?
+
+some_string="Dinesh"
+n=len(some_string)
+print(n)
+characters=n*some_string
+print(characters)
