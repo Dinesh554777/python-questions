d56030481132e4fbc8adae251de3b5372db1c699
DINESH <dinesh3212001@gmail.com>
2026-06-27 11:59:41 +0530

    take input as floating value instead of integer value how its checks and throw the error

--- gokul/week1/official-questions/AQ/19.py
+++ gokul/week1/official-questions/AQ/19.py
@@ -3,4 +3,5 @@
 x=int(input())
 
 # What happens if the input is 1.23 ? Execute and observe the output. Why do you think this is happening?
-# 
+
+#the int() strictly follows the integer value as input of 0 to 9 when entering 1.23 it checks and throw the error
