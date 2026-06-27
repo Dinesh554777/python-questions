e16f4a411db12e21de46572a81561eb45aef827d
DINESH <dinesh3212001@gmail.com>
2026-06-27 11:56:16 +0530

    fix: add missing newline and correct comment for input handling in multiplication

--- gokul/week1/official-questions/AQ/18.py
+++ gokul/week1/official-questions/AQ/18.py
@@ -1,2 +1,4 @@
 # What is the following code snippet trying to do? Try to relate this to the previous question.
 print(float(input())*float(input()) * float(input()))
+
+#take floatinf value input as one by one and then perform product
