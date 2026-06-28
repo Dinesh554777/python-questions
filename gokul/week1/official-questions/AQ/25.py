2635aed0e7ed321926e9d4ea35dee61dbc83e94f
DINESH <dinesh3212001@gmail.com>
2026-06-28 11:28:25 +0530

    feat: implement perfect square checker with user input

--- gokul/week1/official-questions/AQ/25.py
+++ gokul/week1/official-questions/AQ/25.py
@@ -1,1 +1,12 @@
 # Accept a positive integer as input and print True if it is a perfect square and False otherwise. For example, if the input is 25 , then you must print True . If the input is 15 , then you must print False
+
+import math
+
+num = int(input("Enter a positive integer: "))
+
+
+root = math.isqrt(num)
+if root * root == num:
+    print("True")
+else:
+    print("False")
