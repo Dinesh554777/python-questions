297e77aedab0a45cf6ae4da4161d58f2319e4d3c
DINESH <dinesh3212001@gmail.com>
2026-06-28 11:25:16 +0530

    feat: add functionality to sum digits of two and three digit numbers

--- gokul/week1/official-questions/AQ/24.py
+++ gokul/week1/official-questions/AQ/24.py
@@ -1,1 +1,7 @@
 # Accept a two digit number as input and print the sum of its digits. What about a three digit number?
+
+two_digit_num= input("Enter a two digit number: ")
+print(int(two_digit_num[0]) + int(two_digit_num[1]))
+
+three_digit_num=input("Enter a three digit number:")
+print(int(three_digit_num[0])+int(three_digit_num[1])+int(three_digit_num[2]))
