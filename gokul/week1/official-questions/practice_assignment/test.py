b43718f529c05a97be2faefed93a2c6441adee35
DINESH <dinesh3212001@gmail.com>
2026-06-29 11:25:57 +0530

    feat: update markdown with answers and enhance test script with additional print statements

--- gokul/week1/official-questions/practice_assignment/test.py
+++ gokul/week1/official-questions/practice_assignment/test.py
@@ -4,3 +4,9 @@ print('15 % 3 * 2 + 2')
 
 print('1 2 3 4 5')
 print(1,2,3,4,5)
+
+print("1:", type(13 % 5 // 2 * 30 ** 5))
+print("2:", type("@Python"))
+print("3:", type(20 ** 10 / 2 + 25 - 70))
+print("4:", type(False))
+print("5:", type(20 * 100.0 // 11 % 5))
