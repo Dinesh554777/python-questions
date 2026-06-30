038e41a310b3fb55cfb01cf3624634c197507c8e
DINESH <dinesh3212001@gmail.com>
2026-06-30 11:40:21 +0530

    feat: correct spelling and improve clarity in keyword demonstration in Python

--- gokul/week2/lect-2.4/2.4.5.py
+++ gokul/week2/lect-2.4/2.4.5.py
@@ -1,5 +1,8 @@
-# choose which of the following are not a pre defined keyword in python
+# choose which of the following are not a pre defined keyword in python -> are and where
+
 # print all the keywords in first line 
+print("Keyowrd in python :and,is,not,while,for,if,try,catch,except")
 # and all the non keywords in the second line
+print("Non keywords in python : are,where")
 
 # and,are,is,not,while,where,for,if,try,catch,except
