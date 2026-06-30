9b09f0cd31b313687236094379a5e5f39e26f988
DINESH <dinesh3212001@gmail.com>
2026-06-30 11:36:30 +0530

    feat: add demonstration of chaining operators with conditional checks and string methods in Python

--- gokul/week2/lect-2.4/2.4.4.py
+++ gokul/week2/lect-2.4/2.4.4.py
@@ -1,1 +1,15 @@
 # Chaining Operator
+
+#using and operation
+age = 25
+if age >= 18 and age < 30:
+    print("Eligible")
+
+#using methods
+text = "  hello world  "
+
+
+clean_text = text.strip().upper().replace(" WORLD","PYTHON ")
+
+print(clean_text)  # Output: "HELLO PYTHON"
+
