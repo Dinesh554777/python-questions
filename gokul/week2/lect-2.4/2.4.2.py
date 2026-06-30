f7a9006106f8b7be79f51464c998c6fd20d954f9
DINESH <dinesh3212001@gmail.com>
2026-06-30 11:28:06 +0530

    feat: update variable operations to use shorthand notation in Python demonstration

--- gokul/week2/lect-2.4/2.4.2.py
+++ gokul/week2/lect-2.4/2.4.2.py
@@ -1,8 +1,20 @@
 # Write a Python program that initializes a variable x with the value 10 and then 
 # performs the following operations using shorthand operators:
 
+x=10
 # Add 3 to x.
+x=x+3
+print(x)
+
 # Subtract 4 from x.
+x=x-4
+print(x)
+
 # Multiply x by 2.
+x=x*2
+print(x)
+
 # Divide x by 3.
+x=x/3
+print(x)
 # Print the value of x after each operation.
