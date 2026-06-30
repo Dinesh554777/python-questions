5f1f87b086a01a08fe8b527ae1eef6141d8b9a2c
DINESH <dinesh3212001@gmail.com>
2026-06-30 11:52:20 +0530

    fix: add inline comments to clarify output in Python example

--- gokul/week2/lect-2.4/2.4.14.py
+++ gokul/week2/lect-2.4/2.4.14.py
@@ -1,5 +1,5 @@
 # predict the output
 
-print("alpha" in "i am in alpha squad")
-print("beta" in "i am in betasquad")
-print("gamma" in "i am in gamasquad")
+print("alpha" in "i am in alpha squad") #alpha is in the string true 
+print("beta" in "i am in betasquad")  #beta is in the string true
+print("gamma" in "i am in gamasquad") #gama is not matches the gamma so it is false
