df81ebe0dbbe56b5fe8ba6d6c89e0eaf936a3a63
DINESH <dinesh3212001@gmail.com>
2026-06-30 11:42:24 +0530

    feat: uncomment code to demonstrate variable naming restrictions in Python

--- gokul/week2/lect-2.4/2.4.6.py
+++ gokul/week2/lect-2.4/2.4.6.py
@@ -1,6 +1,9 @@
 # predict the output
 
-'''
-and=24
-print(and+6)
-'''
+
+# and=24 # cannot use keyword as variable name 
+# print(and+6) #invalid syntax
+ 
+#correct variable declaration
+a=24
+print(a+6)
